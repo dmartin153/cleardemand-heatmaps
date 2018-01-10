57edcccfae787ca035999d606f2515e4650dbe38
dmartin153 <david.martin@my.wheaton.edu>
2018-01-10 13:00:07 -0700

    separated basic heatmap & correlation plots

--- plotter.py
+++ plotter.py
@@ -5,36 +5,37 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import os
+import pdb
 
 
-def build_heatmap(df, index, column, value, sortby=None):
+def build_basic_heatmap(df, index, column, value):
     '''This function builds a heatmap to look at the given value over various
     index and columns. Sorts by the sortby value, defaulting to the value.
     Inputs:
     df -- pandas dataframe with the data
     index -- column to use as the index of the pivot table
     column -- column to use as the columns of the pivot table
-    value -- value to plot on the heatmap
-    sortby -- value to use to sort the heatmap, defaults to value
+    value -- value to plot on the heatmap, also what the heatmap is sorted by
     Outputs:
     None
+    Saves a jpeg in figures/basic_heatmaps/
     By: David Martin
     On: Jan 10, 2018'''
-    if sortby==None:
-        sortby=value
+    sns.set()
     name = '{val}_heatmap__{ind}_vs_{col}'.format(val=value, ind=index, col=column)
-    pt = df.pivot_table(index=index, columns=column, values=[value,sortby], aggfunc=np.mean)
-    main_column = np.argmax(df.groupby(column)[sortby].sum())
-    main_index = np.argmax(df.groupby(index)[sortby].sum())
-    pt.sort_values(by=main_column, axis=0, inplace=True)
-    pt.sort_values(by=main_index, axis=1, inplace=True)
+    pt = df.pivot_table(index=index, columns=column, values=value, aggfunc=np.mean)
+    main_column = np.argmax(df.groupby(column)[value].sum())
+    main_index = np.argmax(df.groupby(index)[value].sum())
+    pt.sort_values(by=main_column, axis=0, inplace=True, ascending=True, na_position='first')
+    pt.sort_values(by=main_index, axis=1, inplace=True, ascending=True, na_position='first')
     fig = plt.figure()
     ax = sns.heatmap(pt)
     ax.set_yticklabels(ax.get_yticklabels(),rotation=0)
     ax.set_xticklabels(ax.get_xticklabels(),rotation=90)
     ax.set_title(name.replace('_',' '))
     fig.tight_layout()
-    saveloc='figures/heatmaps/'
+    saveloc='figures/basic_heatmaps/'
+    check_dir(saveloc)
     fig.savefig(saveloc+name+'.jpg')
     plt.close(fig)
 
