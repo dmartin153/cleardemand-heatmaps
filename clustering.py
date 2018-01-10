855d4b6ee20f6c2043d2acd1bff5a155930ae7a4
dmartin153 <david.martin@my.wheaton.edu>
2018-01-10 13:12:51 -0700

    standardized pca

--- clustering.py
+++ clustering.py
@@ -1,18 +1,22 @@
-'''This module contains code related to clustering'''
+'''This module contains code related to clustering models'''
 import numpy as np
 import pandas as pd
 import pdb
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from scipy.cluster import hierarchy
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.decomposition import PCA
 
 def kmeans_cluster(df,cols=['Cost'],n_clusters=3):
     '''This function builds and returns a basic Kmeans cluster for the provided
-    dataframe and columns. Inputs:
+    dataframe and columns.
+    Inputs:
     df -- pandas dataframe with the data
-    cols -- array of columns to use for clustering, defaults to using just the Cost column'''
+    cols -- array of columns to use for clustering, defaults to using just the Cost column
+    Outputs:
+    model -- a fit kmeans cluster built with the provided parameters'''
     kmeans = KMeans(n_clusters=n_clusters)
     X = np.array(df[cols])
     kmeans.fit(X)
@@ -20,10 +24,17 @@ def kmeans_cluster(df,cols=['Cost'],n_clusters=3):
 
 def elbow_plot(df,cols=['Cost']):
     '''This function makes an elbow plot to evaluate the optimal number of clusters
-    using Within Cluster Sum of Squares'''
+    using Within Cluster Sum of Squares
+    Inputs:
+    df -- pandas dataframe with the data
+    cols -- array of columns to use for clustering, defaults to using just the Cost column
+    Outputs:
+    None'''
     sns.set
     wcss = []
+    scaler = StandardScaler()
     X = np.array(df[cols])
+    X = scaler.fit_transform(X)
     for i in range(1,11):
         model = KMeans(n_clusters=i,random_state=1)
         model.fit(X)
@@ -35,7 +46,9 @@ def hierarchical_cluster(df,cols=['Cost'], n_clusters=3):
     dataframe and columns.
     Inputs:
     df -- pandas dataframe with the  data
-    cols == array of  columns to use for clustering, defaults to using just the Cost column'''
+    cols -- array of  columns to use for clustering, defaults to using just the Cost column
+    Outputs:
+    model -- a fit Agglomerative cluster built with the provided parameters'''
     hier = AgglomerativeClustering(n_clusters=n_clusters)
     X = np.array(df[cols])
     hier.fit(X)
@@ -55,4 +68,19 @@ def perform_pca(df,cols=None,n_components=3):
     else:
         X = df.select_dtypes([int,float]).fillna(0).values
     pca.fit(X)
+    return pca
+
+def explained_variance(df,cols=None):
+    '''this function makes a plot for explained total variance of the dataframe
+    for varying numbers of pca components kept.'''
+    pca = PCA(n_components=len(cols))
+    scaler = StandardScaler()
+    X = df[cols].values
+    stand_X = scaler.fit_transform(X)
+    pca.fit(stand_X)
+    fig = plt.figure()
+    plt.plot(np.cumsum(pca.explained_variance_)/np.sum(pca.explained_variance_),'.')
+    plt.xlabel('Principle Components (#)')
+    plt.ylabel('Expained Variance (proportion)')
+    fig.show()
     return pca
