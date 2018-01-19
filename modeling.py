4b85af6f77cad1bf46f3a2911d80fab4509d787c
dmartin153 <david.martin@my.wheaton.edu>
2018-01-18 17:47:03 -0700

    Added isolation forest

--- modeling.py
+++ modeling.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pdb
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import IsolationForest
 
 def feature_cleaning(df,cols,target):
     '''This function is used to divide the provided data into an X feature matrix
@@ -22,3 +23,30 @@ def build_regression_model(df,model=LinearRegression(),features=['NumIdenticalPr
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=rs)
     model.fit(X_train,y_train)
     return model, X_train, y_train, X_test, y_test
+
+def iso_forest_predict_outliers(df):
+    '''This function predicts outliers using an isolation forest and the CurPriceStdVariation'''
+    iso_forest = IsolationForest(n_jobs=-1, random_state=22,contamination=0.0001)
+    outliers = []
+    for prod in df['ProductId'].unique(): # for each product
+        selection_matrix = df['ProductId'] == prod
+        X = df[selection_matrix]['CurPriceStdVariation'].values
+        weights = df[selection_matrix]['CurRev'].values
+        X = X.reshape(-1, 1)
+        iso_forest.fit(X,sample_weight=weights)
+        preds = iso_forest.predict(X)
+        outlier_inds = df[df['ProductId'] == prod][preds==-1].index
+        outliers.append(outlier_inds)
+    return outliers
+
+def build_new_suggested_prices(df,outliers):
+    '''This function creates a new column of IsoSugPrice, which is based off the isolation
+    forest'''
+    dct = {}
+    df['IsoSugPrice'] = df['CurPrice'] #Initialize as current price
+    for outlier in outliers:
+        df.loc[outlier, 'IsoSugPrice'] = df.loc[outlier, 'IsoSugPrice'] - df.loc[outlier, 'CurPriceVariation'] / 2
+
+def add_iso_sug_price(df):
+    outliers = iso_forest_predict_outliers(df)
+    build_new_suggested_prices(df,outliers)
